daf5e20e1bec6e3f40cd6a8fa12b4dfd3b08efdf
rtutra21 <artur2003449@gmail.com>
2026-02-20 16:46:34 +0300

    Тестирование репозитория через CommandLineRunner

--- src/main/java/com/todolist/toDoList/ToDoListApplication.java
+++ src/main/java/com/todolist/toDoList/ToDoListApplication.java
@@ -10,4 +10,40 @@ public class ToDoListApplication {
 		SpringApplication.run(ToDoListApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo(TaskRepository repository) {
+		return args -> {
+			Task task1 = Task.builder() // создание первой таски на репозитории
+					.title("Дедлайн по проекту")
+					.description("Сдать пет-проект до конца месяца")
+					.status(Status.NEW)
+					.priority(Priority.HIGH)
+					.dueDate(LocalDate.of(2025, 12, 10))
+					.build();
+
+			Task task2 = Task.builder() // создание второй таски на репозитории
+					.title("Празднование дня рождения")
+					.description("У друга отмечается день рождения")
+					.status(Status.IN_PROGRESS)
+					.priority(Priority.MEDIUM)
+					.dueDate(LocalDate.of(2025, 01, 01))
+					.build();
+
+			repository.save(task1); // сохранение задач в репозитории
+			repository.save(task2);
+
+			System.out.println("Все задачи на сегодня:");
+			repository.findAll()
+					.forEach(task -> System.out.printf
+							("id: %d, title: %s, description: %s, status: %s, priority: %s, dueDate: %s%n",
+									task.getId(),
+									task.getTitle(),
+									task.getDescription(),
+									task.getStatus(),
+									task.getPriority(),
+									task.getDueDate()));
+
+		};
+	}
+
 }
